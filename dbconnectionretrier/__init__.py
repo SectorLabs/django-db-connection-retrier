8b0b11c889bdebed88b40ff07b3f33acce061fda
Swen Kooij <swen@sectorlabs.ro>
2021-11-08 14:30:51 +0200

    Do not set default_app_config for Django 3.2 and newer
    See: https://docs.djangoproject.com/en/3.2/releases/3.2/#what-s-new-in-django-3-2

--- dbconnectionretrier/__init__.py
+++ dbconnectionretrier/__init__.py
@@ -1,1 +1,4 @@
-default_app_config = "dbconnectionretrier.apps.DBConnectionRetrierConfig"
+import django
+
+if django.VERSION < (3, 2):  # pragma: nocover
+    default_app_config = "dbconnectionretrier.apps.DBConnectionRetrierConfig"
